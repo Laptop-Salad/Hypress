7b7b5ee925ea4fe4fda881a74ef626789f2d7557
Laptop-Salad <awallis1609@gmail.com>
2025-02-22 20:17:27 +0000

    add pipelines

--- app/Livewire/Globe.php
+++ app/Livewire/Globe.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\SubseaPipeline;
 use Livewire\Component;
 
 class Globe extends Component
 {
+    public function assets() {
+        $pipelines = SubseaPipeline::all();
+
+        return $pipelines;
+    }
+
     public function render() {
         return view('livewire.globe');
     }
