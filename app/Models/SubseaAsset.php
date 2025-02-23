5210023bfdd0084735e48c6f10f941aa0bf368ae
Laptop-Salad <awallis1609@gmail.com>
2025-02-23 05:02:07 +0000

    fix: pipelinehealth cast on subseasset

--- app/Models/SubseaAsset.php
+++ app/Models/SubseaAsset.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\PipelineHealth;
 use Illuminate\Database\Eloquent\Model;
 
 class SubseaAsset extends Model
@@ -11,6 +12,7 @@ class SubseaAsset extends Model
     protected $casts = [
         'coordinates' => 'json',
         'last_inspection' => 'date',
-        'next_maintenance' => 'date'
+        'next_maintenance' => 'date',
+        'health' => PipelineHealth::class,
     ];
 }
